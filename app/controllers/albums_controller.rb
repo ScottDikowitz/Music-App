6b8e70f621ebd2e6f97a796b1380b3c64dc6e1d3
= <=>
2015-10-22 17:58:27 -0400

    create new album view and set appropriate default option

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,4 +2,22 @@ class AlbumsController < ApplicationController
   def show
     @album = Album.find(params[:id])
   end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to band_url(@album.band_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @band_id = params[:band_id]
+    @bands = Band.all
+  end
+
+  def album_params
+    self.params.require(:album).permit(:name, :band_id)
+  end
 end
